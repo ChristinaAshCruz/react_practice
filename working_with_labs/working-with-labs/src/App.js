586b233fa545607a97bbe7b76fd894fd05ebe9d2
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-05-31 22:55:21 -0700

    implemented states, props, and events

--- working_with_labs/working-with-labs/src/App.js
+++ working_with_labs/working-with-labs/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  );
+};
+
+const App = () => {
+  // const name = null;
+  const [counter, setCounter] = useState(0);
+
+  // a useEffect takes in 2 parameters
+  useEffect(() => {
+    // this allows us to control an effect
+    // everything in the first paramater happens onLoad
+    // setCounter(100);
+    alert("You've changed the counter to " + counter);
+  }, [
+    // everything here happens after Load
+    counter,
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name={"Johnny"} lastName={"Doe"} age={50} />
+      {/* for your event, add your callback function inside the brackets */}
+      {/* with your callback function, you can now manipulate your counter variable */}
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
-}
+};
 
 export default App;
